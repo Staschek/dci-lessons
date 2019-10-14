82bd5c6e09766ac1594188143b17659728c87dea
Staschek <staschek@protonmail.com>
2019-10-14 14:19:33 +0200

    failue redone

--- 3- Js/14.10.19/main.js
+++ 3- Js/14.10.19/main.js
@@ -1,74 +1,29 @@
-// // Secret Password
-// let passWord = '$3cr37p4ssw0rd';
-
-
-// function greet(){
-//     let name = prompt("Please enter your name:");
-//     if(name == "" || name == null){
-//         txt = "There is no name!"     
-//     }else{
-//         txt = `Hello ${name}`
+// function  visitthecity (city){
+//     if (city = "Hamburg"){
+//         return `you have visited ${city}`;}
 //     }
-//     document.write(txt);
-// }
+//  document.write(visitthecity(citi))
 
-// greet()
+let citySearch = ["Hamburg", "Frankfurt", "Accra"]
+let citiesList = ["Hamburg","Istanbul","Frankfurt", "Berlin", "Kopenhagen"]
 
-
-// let passW = prompt("Please enter your Password!")
-// function checkPW(pw){
-//     let dolla = false;
-//     let counter = 0;
-//     if(pw.length >= 8){
-//         for(let i = 0; i < pw.length; i++){
-// //counter for seeing if the condition is breaking after finding Sdolla ($)
-//             counter++
-        
-//             if(pw[i] === '$'){
-//                 dolla = true;
-//                 break;
-//             }
-//         }
-//     }else{
-//         document.write("<br><br>Your Passwort is less than 8 Characters")
-//         return false;
-//     //return allways ends the function
-//     }
-//     console.log("Count is " + counter)
-
-//     if(dolla){
-//         document.write("<br><br>Your Password is Valid")
-//     }else{
-//         document.write("<br><br>Your Password has no *Dolla* sign")}
-
-//  return dolla;
+function cityIveVisited(citiS, citiV){
+    let temp = [];
+    let matches = false;
     
-// }
+    for(i=0; i < citiV.length; i++)
+    {matches = false;
+        for(j=0; j < citiS.length; j++)
+            {if (citiV[i] === citiS[j])
+                {matches = true;
+                }
+            }
+    // Check if matches is true. If matches == false, u'll see the unvisited Cities
+        if(matches == true)
+            {temp.push(citiV[i])
+        }
+    }
+    return `You have visited: ${temp}`
+}
 
-
-// console.log(checkPW(passW));
-
-// ///////////////////////////////////////////////////////7
-
-
-// let paragraph = "the quick brown fox jumps pver the lazy dog." + 
-// "If the dog barked, was it really lazy?";
-// let str = prompt("search for an index of :");
-
-// let result = `Yes it is found in the positin: ${paragraph.indexOf(str)}` 
-// document.write(result)
-
-// function searchForLetter(pW,ltr){
-//     if(pW.indexOf(ltr) == 0){
-//         return `This ${ltr} is not found in the given Password`
-//     }else{
-//         return `Yes it is found in the position : ${pW.indexOf(ltr)+1}`
-//     }
-// }
-// let ltr1 = prompt('Search in my password for this letter :');
-// let pass = "secret&2020";
-// let result1 = searchForLetter(pass,ltr1);
-// document.write(result1);
-
-///////////////////////////////////////////////
-
+document.write(cityIveVisited(citySearch, citiesList))
--- 3- Js/24.09.19/main.js
+++ 3- Js/24.09.19/main.js
@@ -1,29 +1,74 @@
-// function  visitthecity (city){
-//     if (city = "Hamburg"){
-//         return `you have visited ${city}`;}
+// // Secret Password
+// let passWord = '$3cr37p4ssw0rd';
+
+
+// function greet(){
+//     let name = prompt("Please enter your name:");
+//     if(name == "" || name == null){
+//         txt = "There is no name!"     
+//     }else{
+//         txt = `Hello ${name}`
 //     }
-//  document.write(visitthecity(citi))
+//     document.write(txt);
+// }
 
-let citySearch = ["Hamburg", "Frankfurt", "Accra"]
-let citiesList = ["Hamburg","Istanbul","Frankfurt", "Berlin", "Kopenhagen"]
+// greet()
 
-function cityIveVisited(citiS, citiV){
-    let temp = [];
-    let matches = false;
+
+// let passW = prompt("Please enter your Password!")
+// function checkPW(pw){
+//     let dolla = false;
+//     let counter = 0;
+//     if(pw.length >= 8){
+//         for(let i = 0; i < pw.length; i++){
+// //counter for seeing if the condition is breaking after finding Sdolla ($)
+//             counter++
+        
+//             if(pw[i] === '$'){
+//                 dolla = true;
+//                 break;
+//             }
+//         }
+//     }else{
+//         document.write("<br><br>Your Passwort is less than 8 Characters")
+//         return false;
+//     //return allways ends the function
+//     }
+//     console.log("Count is " + counter)
+
+//     if(dolla){
+//         document.write("<br><br>Your Password is Valid")
+//     }else{
+//         document.write("<br><br>Your Password has no *Dolla* sign")}
+
+//  return dolla;
     
-    for(i=0; i < citiV.length; i++)
-    {matches = false;
-        for(j=0; j < citiS.length; j++)
-            {if (citiV[i] === citiS[j])
-                {matches = true;
-                }
-            }
-    // Check if matches is true. If matches == false, u'll see the unvisited Cities
-        if(matches == true)
-            {temp.push(citiV[i])
-        }
-    }
-    return `You have visited: ${temp}`
-}
+// }
 
-document.write(cityIveVisited(citySearch, citiesList))
+
+// console.log(checkPW(passW));
+
+// ///////////////////////////////////////////////////////7
+
+
+// let paragraph = "the quick brown fox jumps pver the lazy dog." + 
+// "If the dog barked, was it really lazy?";
+// let str = prompt("search for an index of :");
+
+// let result = `Yes it is found in the positin: ${paragraph.indexOf(str)}` 
+// document.write(result)
+
+// function searchForLetter(pW,ltr){
+//     if(pW.indexOf(ltr) == 0){
+//         return `This ${ltr} is not found in the given Password`
+//     }else{
+//         return `Yes it is found in the position : ${pW.indexOf(ltr)+1}`
+//     }
+// }
+// let ltr1 = prompt('Search in my password for this letter :');
+// let pass = "secret&2020";
+// let result1 = searchForLetter(pass,ltr1);
+// document.write(result1);
+
+///////////////////////////////////////////////
+
